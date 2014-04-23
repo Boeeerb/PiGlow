ff32c456640a1c9f8fe6b1a0b1c88f244ab1b8d7
Boeeerb <jase@boeeerb.co.uk>
2014-04-23 07:14:49 +0100

    Added setup.py
    Prep for python pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name	= 'PiGlow',
+version	= '0.4',
+author	= 'Jason Barnett',
+author_email	= 'jase@boeeerb.co.uk',
+description	= 'A module to control the PiGlow Raspberry Pi Addon Board',
+long_description= open('README.md').read(),
+license	= 'MIT',
+keywords	= 'Raspberry Pi PiGlow',
+url	= 'http://www.boeeerb.co.uk',
+classifiers = classifiers,
+py_modules	= ['piglow'],
+install_requires= ['rpi.gpio >= 0.5.4']
+)
